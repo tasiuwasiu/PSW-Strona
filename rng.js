abb862e8f2b70dcb62458951c87bc8b297626937
Rafał Wasik <228034@student.pwr.edu.pl>
2017-11-15 21:26:54 +0100

    Ukończona lista JS

---
+++ rng.js
@@ -0,0 +1,9 @@
+function rng() {
+		var max;
+		do{
+		max = parseInt(window.prompt("Problem z podziałem itemów? Wylosuj liczbę!  Podaj max wartość: ", "100"));
+		}while(isNaN(max) || max < 1);
+	
+		var rand = Math.random()*max;
+		document.getElementById("RNG").innerHTML = Math.floor(rand);
+	}
